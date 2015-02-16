902566575a534b2fdf928c25f3d2d09f3cfe724f
Peter Haza <peter.haza@gmail.com>
2015-02-16 09:14:41 +0100

    Fix array assignment of WSA options + code reformat

--- src/WSACurlSoapClient.php
+++ src/WSACurlSoapClient.php
@@ -9,26 +9,24 @@ use WS\WSASoap;
 */
 class WSACurlSoapClient extends CurlSoapClient
 {
-
-  const ADD_ACTION    = "addaction";
-  const ADD_TO        = "addto";
+  const ADD_ACTION     = "addaction";
+  const ADD_TO         = "addto";
   const ADD_MESSAGE_ID = "addmessageid";
   const ADD_REPLY_TO   = "addreplyto";
 
-  
-  protected $wseOptions = [
-    self::ADD_ACTION => true,
-    self::ADD_TO => true,
+  protected $wsaOptions = [
+    self::ADD_ACTION     => true,
+    self::ADD_TO         => true,
     self::ADD_MESSAGE_ID => true,
-    self::ADD_REPLY_TO => true
+    self::ADD_REPLY_TO   => true
   ];
   
-  function __construct($wsdl, array $options, array $curlOptions, Logger $logger, array $wseOptions = []) {
-    $this->wseOptions += $wseOptions;
+  function __construct($wsdl, array $options, array $curlOptions, Logger $logger, array $wsaOptions = []) {
+    $this->wsaOptions = $wsaOptions + $this->wsaOptions;
     
     parent::__construct($wsdl, $options, $curlOptions, $logger);
   }
-  
+
   /**
    * We override this function from parent to use WS-Security.
    *
@@ -41,27 +39,27 @@ class WSACurlSoapClient extends CurlSoapClient
    * @author Peter Haza
    */
   public function __doRequest($request, $location, $action, $version, $one_way = 0) {
-    
+
     $dom = new \DOMDocument();
     $dom->loadXML($request);
-    
+
     $wsa = new WSASoap($dom);
-    
-    if($this->wseOptions[self::ADD_ACTION]) {
+
+    if($this->wsaOptions[self::ADD_ACTION]) {
       $wsa->addAction($action);
     }
-    if($this->wseOptions[self::ADD_TO]) {
+    if($this->wsaOptions[self::ADD_TO]) {
       $wsa->addTo($location);
     }
-    if($this->wseOptions[self::ADD_MESSAGE_ID]) {
+    if($this->wsaOptions[self::ADD_MESSAGE_ID]) {
       $wsa->addMessageID();
     }
-    if($this->wseOptions[self::ADD_REPLY_TO]) {
+    if($this->wsaOptions[self::ADD_REPLY_TO]) {
       $wsa->addReplyTo();
     }
 
     $request = $wsa->saveXML();
     
-    return parent::__doRequest($request, $location, $action, $version, $one_way);    
+    return parent::__doRequest($request, $location, $action, $version, $one_way);
   }
 }
